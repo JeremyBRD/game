79ad0e11664f35faf80d277702d7b0c8afef1503
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-09-02 20:40:30 +0200

    missing to add rooms and break point

--- lib/game.rb
+++ lib/game.rb
@@ -1,27 +1,30 @@
+# STEP 1 define when the game is starting
 def new_game(user_answer)
-  if user_answer == "yes" || "y"
+  if user_answer == "yes" || user_answer == "y"
     ""
-  else user_answer == "no" || "n"
-    exit
+  else user_answer == "no" || user_answer == "n"
+    "exit"
   end
   puts "Loading a new game"
   puts "..."
 end
 
+# STEP 2 differents routes for the user
 def action (user_action)
-  if user_action == "right"
+  if user_action == "right" || user_action == "^[[C"
     @current_position = @current_position + 1
     puts "Moving on the right you are here : #{@current_position}"
-  elsif user_action == "left"
+  elsif user_action == "left" || user_action == "^[[D"
     @current_position = @current_position - 1
     puts "Moving on the left you are here : #{@current_position}"
   elsif user_action == "fight"
-    "Let's choose a weapon ! 🚀"
+    puts "Let's choose a weapon ! 🚀"
   else
-    "CAN YOU PLEASE CHOOSE AN ACTION WITHIN THE LIST ? 😬"
+    puts "CAN YOU PLEASE CHOOSE AN ACTION WITHIN THE LIST ? 😬"
   end
 end
 
+# STEP 3 define the weapons stuff (display, damage and targeting)
 def display(weapons)
   weapons.each_with_index do |weapon, index|
     puts "[#{index + 1}] - #{weapon}"
@@ -32,9 +35,9 @@ def weapon (user_choice)
   if user_choice == 0
     "🚀 BOOM ! 💥"
   elsif user_choice == 1
-    "🔊 Hallelujah 🔊 BOOM ! 💥"
+    "Pouf BOOM ! 💥"
   elsif user_choice == 2
-    "🐑 Bêêêê 🐑 BOOM ! 💥"
+    "BAM BOOM ! 💥"
   elsif
   user_choice == 3
     "Paf ! 💥"
@@ -42,3 +45,38 @@ def weapon (user_choice)
     "CAN YOU PLEASE CHOOSE A WEAPON WITHIN THE LIST ? 😬"
   end
 end
+
+def damage(weapon, pointing_number)
+  if weapon == 0
+    @range_damaged = [pointing_number, pointing_number+1]
+  elsif weapon == 1
+    @range_damaged = [pointing_number, pointing_number + 1, pointing_number + 2, pointing_number + 3]
+  elsif weapon == 2
+    @range_damaged = [pointing_number, pointing_number + 1, pointing_number + 2]
+  else
+    @range_damaged = [pointing_number]
+  end
+end
+
+def target(bots_position)
+  @range_damaged.each do |damage|
+    if damage == @bot_position["Bot 1"]
+      puts "You killed the bot 1 ! 🙌 he was on position #{@bot1}" && @bot_team.delete("bot1")
+    elsif damage == @bot_position["Bot 2"]
+      puts "You killed the bot 2 ! 🙌 he was on position #{@bot2}" && @bot_team.delete("bot2")
+    elsif damage == @bot_position["Bot 3"]
+      puts "You killed the bot 3 ! 🙌 he was on position #{@bot3}" && @bot_team.delete("bot3")
+    end
+  end
+  if @bot_team.size == 0
+    puts "🏆 YOU WIN !!!! 🏆"
+    puts "******************"
+  end
+end
+
+# STEP 4 define the bot game
+def bot_game(current_position)
+  Array(1..15).sample == current_position ? (puts "You die 😱") : (puts "The bot missed his target !")
+end
+
+# STEP 5 define Game Over or You win !
--- lib/interface.rb
+++ lib/interface.rb
@@ -1,5 +1,5 @@
 require_relative "game"
-# require "readline"
+require "readline"
 
 answer = nil
 @current_position = Array(1..15).sample
@@ -7,19 +7,20 @@ answer = nil
 @bot1 = @bot_map.sample
 @bot2 = @bot_map.sample
 @bot3 = @bot_map.sample
-@weapons = %w(Rocket Hallelujah Sheep Gun)
-@user_team = 
+@weapons = %w(Rocket Uzi Gun Nunchaku)
+@bot_team = %w(bot1 bot2 bot3)
+@bot_position = { "Bot 1" => @bot1, "Bot 2" => @bot2, "Bot 3" => @bot3 }
+@running = true
 
-# while answer != "exit"
-  puts "⭐️ Hello, Welcome to Worms on command line ⭐️"
+  puts "⭐️ Hello, Welcome to SHOT GUN on command line ⭐️"
   puts "           -----------------------           "
-  # sleep 1
+  sleep 1
   puts "This game a is really simple game, for each turn you'll have the choice to move (left or right)"
   puts "..."
-  # sleep 2
-  puts "or choose between 3 weapons in a given list to try to shoot your 3 enemys 😱"
+  sleep 2
+  puts "or choose between the given weapons to try to kill your 3 enemies 😱"
   puts "..."
-  # sleep 2
+  sleep 2
   puts "- What's your user name ?"
   print "> "
   user_name = gets.chomp
@@ -28,22 +29,38 @@ answer = nil
   user_answer = gets.chomp.downcase
   new_game(user_answer)
   
-  # sleep 3
-
+  sleep 3
+while user_answer != "exit"
+  @range_damaged = nil
   # Here start the game, each turn the loop will start again
   puts "- Choose an action : (Left / Right / Fight)"
+  p @bot_position
   print "> "
   user_action = gets.chomp.downcase
   puts action(user_action)
+  
+  if user_action == "left" || user_action == "right"
+    puts "this is the turn of the bot team !"
+    sleep 1.5
+    bot_game(@current_position)
+  end
 
   if user_action == "fight"
     display(@weapons)
     puts "- Choose your Weapon (Type the right number)"
     print "> "
     weapon_index = gets.chomp.to_i - 1
+    puts "Where do you want to shoot ? (Choose a number between 1 & 15)"
+    pointing_number = gets.chomp.to_i
     puts weapon(weapon_index)
-  end
-
-# end
-
-puts "- GoodBye !"
+    puts "..."
+    damage(weapon_index, pointing_number)
+    sleep 1
+    target(@bot_position)
+    sleep 1
+    puts "this is the turn of the bot team !"
+    sleep 1.5
+    bot_game(@current_position)
+  end 
+end
+puts "- GoodBye ! 👋"
