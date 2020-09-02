bc2a70fd01d61579c7810b1fc02bad395215d039
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-09-02 16:02:02 +0200

    init game controller with  a few methods, starting the interface loop

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,53 @@
-current_position = Array(1..15).sample
+def initialize 
+  @current_position = Array(1..15).sample
+  @bot1 = Array(1..15).sample
+  @bot2 = Array(1..15).sample
+  @bot3 = Array(1..15).sample
+  @weapons = %w(Rocket, Hallelujah, Sheep, Gun)
+end
 
-def action (user_answer)
+def new_game(user_answer)
+  if user_answer == "Yes"
+    ""
+  else user_answer == "No"
+    exit
+  end
+  puts "Loading a new game"
+  puts "..."
+  
+end
 
+def action (user_action)
+  if user_action == "right"
+    current_position = current_position + 1
+    puts "Moving on the right"
+  elsif user_action == "left"
+    current_position = current_position - 1
+    puts "Moving on the left"
+  elsif user_action == "fight"
+    "Let's choose a weapon ! 🚀"
+  else
+    "CAN YOU PLEASE CHOOSE AN ACTION WITHIN THE LIST ? 😬"
+  end
+end
+
+def weapon_display
+  @weapons.each_with_index do |weapon, idx|
+    "[#{idx + 1}] - #{weapon}"
+  end
+end
+
+def weapon (user_choice)
+  if user_choice == "Rocket"
+    "🚀 BOOM ! 💥"
+  elsif user_choice == "Hallelujah"
+    "🔊 Hallelujah 🔊 BOOM ! 💥"
+  elsif user_choice == "Sheep"
+    "🐑 Bêêêê 🐑 BOOM ! 💥"
+  elsif
+  user_choice == "Gun"
+    "Paf ! 💥"
+  else
+    "CAN YOU PLEASE CHOOSE A WEAPON WITHIN THE LIST ? 😬"
+  end
 end
--- lib/interface.rb
+++ lib/interface.rb
@@ -6,24 +6,25 @@ sleep 1
 puts "This game a is really simple game, for each turn you'll have the choice to move (left or right)"
 puts "..."
 sleep 2
-puts "or choose between 3 weapons in a given list to try to shoot your 3 enemys"
-puts "..."
-sleep 2
-puts "Be carefull there is a limited number of munitions for a few weapons 😱"
+puts "or choose between 3 weapons in a given list to try to shoot your 3 enemys 😱"
 puts "..."
 sleep 2
 puts "What's your user name ?"
 print "> "
 user_name = gets.chomp
-puts "Ok ! #{user_name} are you ready to start ? (Press enter or type exit)"
+puts "Ok ! #{user_name} are you ready to start ? (Yes/No)"
 print "> "
 user_answer = gets.chomp
+new_game(user_answer)
 
-puts "Loading a new game"
-puts "..."
 sleep 3
 
 # Here start the game, each turn the loop will start again
+
 while input = Readline.readline('> ', true)
+  puts "It's your turn, Choose an action : (Left / Right / Fight)"
+  user_action = gets.chomp
+  puts action(user_action)
+  
   break if input == 'exit'
 end
